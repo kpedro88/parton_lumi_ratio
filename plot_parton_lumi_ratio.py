4f8bb302df641631ffd68413be5075e201c19896
Kevin Pedro <kpedro88@gmail.com>
2023-01-11 16:42:25 -0600

    option to plot absolute lumi instead of ratio

--- plot_parton_lumi_ratio.py
+++ plot_parton_lumi_ratio.py
@@ -15,7 +15,7 @@ def import_attrs(pyname, attrs):
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument("--pdf", type=str, default='NNPDF31_nnlo_as_0118_mc_hessian_pdfas', help="PDF name")
 parser.add_argument("--numer", type=int, required=True, help="cm energy for numerator")
-parser.add_argument("--denom", type=int, required=True, help="cm energy for denominator")
+parser.add_argument("--denom", type=int, default=0, help="cm energy for denominator (default: skip ratio, plot numer directly)")
 parser.add_argument("--yrange", metavar=("ymin","ymax"), type=float, default=(1, 100), nargs=2, help="y axis range (None=auto)")
 args = parser.parse_args()
 
@@ -33,16 +33,20 @@ masses = np.asarray(masses)
 fig, ax = plt.subplots(figsize=(9, 7))
 for ic,combo in enumerate(combos):
     numer_vals = np.asarray(lumis[args.numer][combo])
-    denom_vals = np.asarray(lumis[args.denom][combo])
-    min_length = min(numer_vals.size, denom_vals.size)
-    numer_vals = numer_vals[:min_length]
-    denom_vals = denom_vals[:min_length]
-    mass_vals = masses[:min_length]
-    avoid_0 = denom_vals>0
-    numer_vals = numer_vals[avoid_0]
-    denom_vals = denom_vals[avoid_0]
-    mass_vals = mass_vals[avoid_0]
-    ratio = numer_vals / denom_vals
+    if args.denom>0:
+        denom_vals = np.asarray(lumis[args.denom][combo])
+        min_length = min(numer_vals.size, denom_vals.size)
+        numer_vals = numer_vals[:min_length]
+        denom_vals = denom_vals[:min_length]
+        mass_vals = masses[:min_length]
+        avoid_0 = denom_vals>0
+        numer_vals = numer_vals[avoid_0]
+        denom_vals = denom_vals[avoid_0]
+        mass_vals = mass_vals[avoid_0]
+        ratio = numer_vals / denom_vals
+    else:
+        mass_vals = masses[:numer_vals.size]
+        ratio = numer_vals
     ax.plot(mass_vals, ratio, color=colors[ic], linestyle=lines[ic], label=combo_names[ic])
 
 # axes etc.
@@ -51,10 +55,16 @@ ax.set_xlim(min(masses), max(masses))
 ax.set_xlabel(r"$m_{\mathrm{X}}$ [GeV]")
 ax.set_yscale('log')
 ax.set_ylim(args.yrange[0],args.yrange[1])
-ax.set_ylabel("luminosity ratio ({:g} TeV / {:g} TeV)".format(args.numer/1000, args.denom/1000))
+if args.denom>0:
+    ax.set_ylabel("luminosity ratio ({:g} TeV / {:g} TeV)".format(args.numer/1000, args.denom/1000))
+else:
+    ax.set_ylabel("parton luminosity ({:g} TeV) [pb]".format(args.numer/1000, args.denom/1000))
 ax.legend()
 
 # save
-fname = "parton_lumi_ratio_{}_{}_{}.pdf".format(args.numer, args.denom, args.pdf)
+if args.denom>0:
+    fname = "parton_lumi_ratio_{}_{}_{}.pdf".format(args.numer, args.denom, args.pdf)
+else:
+    fname = "parton_lumi_{}_{}.pdf".format(args.numer, args.pdf)
 plt.savefig(fname, bbox_inches='tight')
 
